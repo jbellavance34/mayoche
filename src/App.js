23bd230b088478b3391d865bda3de997e2671070
jbellavance34 <jbellavance34@gmail.com>
2022-01-16 17:29:12 -0500

    ajout date du vote frontend

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import awsExports from './aws-exports'
 
 Amplify.configure(awsExports);
 
-const initialState = { name: '', description: '', animal: 'cat', type: 'vote'}
+const initialState = { name: '', description: '', animal: 'cat', type: 'vote' }
 
 const App = () => {
 
@@ -22,7 +22,7 @@ const App = () => {
   async function fetchchoices() {
     try {
       const choiceData = await API.graphql(graphqlOperation(votesByDate, {
-        limit: 20,
+        limit: 10,
         sortDirection: "DESC",
         type: "vote"
       },
@@ -38,11 +38,11 @@ const App = () => {
 
   async function addchoice() {
     try {
-      if (!formState.name || !formState.description ||!formState.animal) return
+      if (!formState.name || !formState.description || !formState.animal) return
       const choice = { ...formState }
       setchoices([...choices, choice])
       setFormState(initialState)
-      await API.graphql(graphqlOperation(createChoice, {input: choice}))
+      await API.graphql(graphqlOperation(createChoice, { input: choice }))
     } catch (err) {
       console.log('error creating choice:', err)
     }
@@ -52,73 +52,82 @@ const App = () => {
   return (
     <div style={styles.container}>
 
-    <h1>Liste des 20 derniers choix</h1>
-    <table style={styles.table_td_th}>
-      <thead>
-        <tr style={styles.table_td_th, styles.th}>
-          <th style={styles.table_td_th}>Nom</th>
-          <th style={styles.table_td_th}>Animal</th>
-          <th style={styles.table_td_th}>Description</th>
-        </tr>
-      </thead>
-      {
-        choices.map((choice) => (
-          <tbody style={styles.table_td_th} >
-            {
-              <tr key={choice.id} style={styles.table_td_th} >
-                <td style={styles.table_td_th}>{choice.name}</td>
-                <td style={styles.table_td_th}>{choice.animal}</td>
-                <td style={styles.table_td_th}>{choice.description}</td>
-              </tr>
-            }
-          </tbody>
-        ))
-      }
-    </table>
-    <h2>Sélectionnez votre animal préféré</h2>
-    <input
-      onChange={event => setInput('name', event.target.value)}
-      style={styles.input}
-      value={formState.name}
-      maxLength="50"
-      placeholder="Name"
-      required
-    />
-    <input
-      onChange={event => setInput('description', event.target.value)}
-      style={styles.input}
-      maxLength="150"
-      value={formState.description}
-      placeholder="Description"
-    />
-    <select 
-      onChange={event => setInput('animal', event.target.value)}
-      style={styles.input}
-      value={formState.animal}
-      placeholder="Animal"
-    >
-    <option value="cat" defaultValue>Cat</option>
-    <option value="dog">Dog</option>
-    <option value="pet">Pet</option>
-    </select>
+      <h1>Liste des 10 derniers choix</h1>
+      <table style={styles.table_td_th}>
+        <thead>
+          <tr style={styles.table_td_th, styles.th}>
+            <th style={styles.table_td_th}>Nom</th>
+            <th style={styles.table_td_th}>Animal</th>
+            <th style={styles.table_td_th}>Description</th>
+            <th style={styles.table_td_th}>Date du vote</th>
+          </tr>
+        </thead>
+        {
+          choices.map((choice) => (
+            <tbody style={styles.table_td_th} >
+              {
+                <tr key={choice.id} style={styles.table_td_th} >
+                  <td style={styles.table_td_th}>{choice.name}</td>
+                  <td style={styles.table_td_th}>{choice.animal}</td>
+                  <td style={styles.table_td_th}>{choice.description}</td>
+                  <td style={styles.table_td_th}>
+                    {new Intl.DateTimeFormat("fr-GB", {
+                      year: "numeric",
+                      month: "long",
+                      day: "2-digit"
+                    }).format(new Date(choice.createdAt))}
+                  </td>
 
-    <button style={styles.button} onClick={addchoice}>Create choice</button>
-  </div>
+                </tr>
+              }
+            </tbody>
+          ))
+        }
+      </table>
+      <h2>Sélectionnez votre animal préféré</h2>
+      <input
+        onChange={event => setInput('name', event.target.value)}
+        style={styles.input}
+        value={formState.name}
+        maxLength="50"
+        placeholder="Name"
+        required
+      />
+      <input
+        onChange={event => setInput('description', event.target.value)}
+        style={styles.input}
+        maxLength="150"
+        value={formState.description}
+        placeholder="Description"
+      />
+      <select
+        onChange={event => setInput('animal', event.target.value)}
+        style={styles.input}
+        value={formState.animal}
+        placeholder="Animal"
+      >
+        <option value="cat" defaultValue>Cat</option>
+        <option value="dog">Dog</option>
+        <option value="pet">Pet</option>
+      </select>
+
+      <button style={styles.button} onClick={addchoice}>Create choice</button>
+    </div>
   )
 }
 
 const styles = {
   container: { width: 400, margin: '0 auto', display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  choice: {  marginBottom: 15 },
+  choice: { marginBottom: 15 },
   input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
   choiceName: { fontSize: 20, fontWeight: 'bold' },
   choiceDescription: { marginBottom: 0 },
   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' },
-  table_td_th: { width: '20%', border: '1px solid green', textalign: 'center'},
-  th: { backgroundcolor: 'green', color: 'black'},
-  image_cat: { float: 'left', width: "100", height: "50"},
-  image_dog: { float: 'rigth', width: "100", height: "50"}
-  }
+  table_td_th: { width: '40%', border: '1px solid green', textalign: 'center' },
+  th: { backgroundcolor: 'green', color: 'black' },
+  image_cat: { float: 'left', width: "100", height: "50" },
+  image_dog: { float: 'rigth', width: "100", height: "50" }
+}
 
 
 export default App;
--- src/animals.js
+++ src/animals.js
@@ -2,25 +2,25 @@ import React from 'react'
 
 const Cat = () => {
     return (
-    <div style={styles.container}>
-        <figure>
-            <img src='https://mayoche.s3.amazonaws.com/cat.jpg' alt="cute-cat" style={styles.img}/>
-            <figcaption>Cat of the day</figcaption>
-        </figure>
-        <figure>
-            <img src='https://mayoche.s3.amazonaws.com/dog.jpg' alt="cute-dog" style={styles.img}/>
-            <figcaption>Dog of the day</figcaption>
-        </figure>
-        <figure>
-            <img src='https://mayoche.s3.amazonaws.com/pet.jpg' alt="cute-pet" style={styles.img}/>
-            <figcaption>Pet of the day</figcaption>
-        </figure>
-    </div>
+        <div style={styles.container}>
+            <figure>
+                <img src='https://mayoche.s3.amazonaws.com/cat.jpg' alt="cute-cat" style={styles.img} />
+                <figcaption>Cat of the day</figcaption>
+            </figure>
+            <figure>
+                <img src='https://mayoche.s3.amazonaws.com/dog.jpg' alt="cute-dog" style={styles.img} />
+                <figcaption>Dog of the day</figcaption>
+            </figure>
+            <figure>
+                <img src='https://mayoche.s3.amazonaws.com/pet.jpg' alt="cute-pet" style={styles.img} />
+                <figcaption>Pet of the day</figcaption>
+            </figure>
+        </div>
     )
 }
 const styles = {
     container: { width: 1000, margin: '0 auto', display: 'flex', flex: 1, flexDirection: 'row', justifyContent: 'center', padding: 0 },
-    img: {width: '400px', height: '400px', margin: '0 auto'}
+    img: { width: '400px', height: '400px', margin: '0 auto' }
 }
 
 export default Cat;
